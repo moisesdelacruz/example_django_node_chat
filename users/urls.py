5d1c1071fa49cbeb7f372256ef35b37df47157a8
moisesdelacruz <moises-jesus_15@hotmail.com>
2017-05-01 11:29:18 -0400

    [UPDATE] profile public and private views.

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,5 @@ from users import views
 
 urlpatterns = [
     url(r'^@(?P<slug>[-\w]+)/$', views.UserDetailView.as_view(), name='detail'),
-    url(r'^profile/$', views.ProfileTemplateView.as_view(), name='profile'),
-    url(r'^profile/edit/$', views.ProfileUpdateView.as_view(), name='profile_edit'),
+    url(r'^@(?P<slug>[-\w]+)/edit/$', views.ProfileUpdateView.as_view(), name='profile_edit'),
 ]
--- users/views.py
+++ users/views.py
@@ -20,30 +20,20 @@ class UserDetailView(LoginRequiredMixin, DetailView):
     model = User
     slug_field = 'username'
 
-    def dispatch(self, request, *args, **kwargs):
-        if not request.user.username == self.kwargs['slug']:
-            return super(UserDetailView, self).dispatch(request, *args, **kwargs)
-        return redirect(reverse_lazy('users:profile'))
-
     def get_context_data(self, **kwargs):
         context = super(UserDetailView, self).get_context_data(**kwargs)
         context['publishings'] = Publishing.objects.filter(user=self.object.pk)
         return context
 
 # private profile
-class ProfileTemplateView(LoginRequiredMixin, TemplateView):
-    model = User
-    template_name = 'users/user_detail.html'
-
-    def get_context_data(self, **kwargs):
-        context = super(ProfileTemplateView, self).get_context_data(**kwargs)
-        context['object'] = self.model.objects.get(pk=self.request.user.pk)
-        context['publishings'] = Publishing.objects.filter(user=self.request.user.pk)
-        return context
-
 class ProfileUpdateView(LoginRequiredMixin, UpdateView):
     model = User
     form_class = UserUpdateForm
+
+    def dispatch(self, request, *args, **kwargs):
+        if not self.kwargs['slug'] == request.user.username:
+            return redirect(reverse_lazy('users:detail', args=(self.kwargs['slug'],)))
+        return super(ProfileUpdateView, self).dispatch(request, *args, **kwargs)
 
     def get_object(self):
         return self.model.objects.get(pk=self.request.user.pk)
