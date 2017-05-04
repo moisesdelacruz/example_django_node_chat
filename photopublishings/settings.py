25b8fdfe12625d02d2960d43ea6683403447a138
moisesdelacruz <moises-jesus_15@hotmail.com>
2017-05-04 18:21:31 -0400

    [ADD] secret_key in settings.py

--- photopublishings/settings.py
+++ photopublishings/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('PHOTOPUBLISHINGS_SECRET_KEY')
+SECRET_KEY = '-c77ykal!%=0nbr#8po380!j@0)sonvk_)7)ajtog84bmwwb!p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
