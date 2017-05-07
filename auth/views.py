cd9fad5c24f48c9c162f76972deadb587ec4ffeb
moisesdelacruz <moises-jesus_15@hotmail.com>
2017-05-07 17:13:09 -0400

    [UPDATE] new views login and signup

--- auth/views.py
+++ auth/views.py
@@ -1,70 +1,68 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse_lazy
-from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import logout, login, authenticate
 # mixins
 from django.contrib.auth.mixins import LoginRequiredMixin
 # class based views
-from django.views.generic import View
+from django.views.generic import View, FormView
 # users from
 from users.forms import UserCreateForm
+from auth.forms import AuthForm
 
 # Create your views here.
 
 # Sign up
-class SignupView(View):
+class SignupView(FormView):
+    extra_context = None
+    form_class = UserCreateForm
+    template_name = 'auth/signup.html'
+    success_url = reverse_lazy('publishings:list')
 
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_authenticated():
             return super(SignupView, self).dispatch(request, *args, **kwargs)
         return redirect(reverse_lazy('users:detail', args=(self.request.user.username,)))
 
-    def get(self, request, *args, **kwargs):
-        form = UserCreateForm()
-        return render(request, "auth/signup.html", { 'form': form })
+    def form_valid(self, form):
+        form.save()
+        username = form.cleaned_data.get('username')
+        raw_password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=raw_password)
+        login(self.request, user)
+        return super(SignupView, self).form_valid(form)
 
-    def post(self, request, *args, **kwargs):
-        form = UserCreateForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('/')
 
 # Login
-class LoginView(View):
+class LoginView(FormView):
+    form_class = AuthForm
+    template_name = 'auth/login.html'
+    extra_context = None
+    success_url = reverse_lazy('publishings:list')
 
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_authenticated():
             return super(LoginView, self).dispatch(request, *args, **kwargs)
         return redirect(reverse_lazy('users:detail', args=(self.request.user.username,)))
 
-    def get(self, request, *args, **kwargs):
-        form = AuthenticationForm()
-        return render(request, "auth/login.html", { 'form': form })
-
-    def post(self, request, *args, **kwargs):
-        username = request.POST['username']
-        password = request.POST['password']
+    def form_valid(self, form):
+        username = self.request.POST['username']
+        password = self.request.POST['password']
         user = authenticate(username=username, password=password)
 
         if user is not None:
             if user.is_active:
-                login(request, user)
+                login(self.request, user)
                 # redirect
-                url_next = request.GET.get('next')
+                url_next = self.request.GET.get('next')
                 if url_next is not None:
 					return redirect(url_next)
                 else:
-					return redirect('/')
+					return super(LoginView, self).form_valid(form)
             else:
                 return HttpResponse("Inactive user.")
         else:
-            return redirect('/')
-
-        return render(request)
+            return super(LoginView, self).form_valid(form)
 
 # Logout
 class LogoutView(LoginRequiredMixin, View):
